dbae15271347436577a11eb0af1db050f2cd75bd
Rickypg711 <77697094+Rickypg711@users.noreply.github.com>
2023-02-27 12:31:21 -0800

    quotes

--- server.js
+++ server.js
@@ -8,6 +8,29 @@ const PORT = process.env.PORT || 4001;
 
 app.use(express.static('public'));
 
-app.listen(PORT, () => {
-    console.log(`Example app listening on port ${PORT}`)
-  })
+app.get('/api/quotes/random', (req, res, next) => {
+  const quote = getRandomElement(quotes);
+  res.send({quote: quote});
+});
+
+app.get('/api/quotes', (req, res, next) => {
+  if (!req.query.hasOwnProperty('person')) {
+    res.send({quotes: quotes});
+  } else {
+    const filterQuote = quotes.filter(element => element.person === req.query.person);
+    res.send({quotes: filterQuote});
+  }
+});
+
+app.post('/api/quotes', (req, res, next) => {
+  if (req.query.quote && req.query.person) {
+    const newQuote = {quote: req.query.quote, person: req.query.person};
+    quotes.push(newQuote);
+    res.send({quote: newQuote});
+  }
+  else {
+    res.status(400).send();
+  }
+});
+
+app.listen(PORT, () => {console.log(`Server Quote API listening on port:${PORT}.`)});
